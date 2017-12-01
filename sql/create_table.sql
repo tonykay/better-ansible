3427d81af9329b2753cefab69a70ecd9e6326596
Tok <tony.g.kay@gmail.com>
2017-12-01 11:39:20 -0700

    first commit of re-write of good-ansible

---
+++ sql/create_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE exampleTable (
+    id integer NOT NULL,
+    storyText text,
+    createdDate date
+);
---
+++ sql/populate_table.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE exampleTable;
+INSERT INTO exampleTable VALUES (1, 'Ansible Nightmares', '2014-10-23');
+INSERT INTO exampleTable VALUES (2, 'Ansible for DevOps', '2014-10-23');
