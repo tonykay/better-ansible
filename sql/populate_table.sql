c4d76207eb8163c9b3e4da43945e2d830be3d952
Tok <tony.g.kay@gmail.com>
2017-12-15 09:54:59 -0700

    Moving to roles, pre-flight so far, also moving to global_vars as primary

--- sql/populate_table.sql
+++ sql/populate_table.sql
@@ -1,3 +1,6 @@
 TRUNCATE TABLE exampleTable;
 INSERT INTO exampleTable VALUES (1, 'Ansible Nightmares', '2014-10-23');
 INSERT INTO exampleTable VALUES (2, 'Ansible for DevOps', '2014-10-23');
+INSERT INTO exampleTable VALUES (3, 'Ansible Nightmares', '2014-10-23');
+INSERT INTO exampleTable VALUES (4, 'Ansible Nightmares', '2014-10-23');
+INSERT INTO exampleTable VALUES (5, 'Ansible Nightmares', '2014-10-23');
